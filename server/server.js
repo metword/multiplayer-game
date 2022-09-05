7283a1ed84ee903b65d88e05b727da19901caab6
metword <martincgruber@gmail.com>
2022-09-04 21:48:14 -0500

    test

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const httpServer = http.createServer();
 const io = new socket_io.Server(httpServer, {
   cors: {
     origin: "https://glistening-croissant-ee4e9d.netlify.app",
+    credentials: true,
   },
 });
 
