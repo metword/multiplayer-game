a119aff107ec5e21376d76471ff6e4341fed4805
metword <martincgruber@gmail.com>
2022-09-07 21:47:26 -0500

    changed sprite and added gun

--- client/client.js
+++ client/client.js
@@ -1,7 +1,9 @@
 const socket = io();
+
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 const tps = 1000 / 60;
+const color = "rgb(0,255,0)";
 
 let clientId = undefined;
 let serverEntities = null;
@@ -13,24 +15,23 @@ const keyPresses = {
     right : false
 }
 
-class Entity {
+class Vec {
     x;
     y;
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        //console.log(`CREATING ENTITY AT (${x}, ${y})`)
     }
 }
-const clientEntity = new Entity(0,0);
-const velocity = {
-    x : 0,
-    y : 0,
-}
+
+const clientPos = new Vec(0,0);
+const velocity = new Vec(0,0);
+const mousePos = new Vec(0,0);
+let mouseAngle = 0;
 
 window.onload = window.onresize = function () {
-    canvas.height = window.innerHeight-48;
-    canvas.width = window.innerWidth-12;
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
 }
 
 // CLIENT DUTY!!!
@@ -48,23 +49,70 @@ function clientLoop() {
 setInterval(clientLoop, tps);
 
 function render() {
-    //rendering the player
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.rect(clientEntity.x,clientEntity.y,50,50);
-    ctx.fill();
+
+    clearCanvas();
 
     //renders provided by the server
     if (serverEntities !== null) {
         //console.log("SERVER ENTITIES !== NULL")
         for (const entity of serverEntities) {
             if (entity.id !== clientId  && entity.position !== null) {
-                ctx.beginPath();
-                ctx.rect(entity.position.x,entity.position.y,50,50);
-                ctx.fill();
+                drawPlayer(entity.position.x, entity.position.y, -entity.mouseAngle ,"rgb(255,0,0)");
             }
         }
     }
+
+    drawPlayer(clientPos.x,clientPos.y, -mouseAngle, color);
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+//////////////////
+//  RENDERING   //
+//////////////////
+function drawPlayer(x, y, angle, color) {
+    ctx.save();
+    ctx.translate(x,y);
+    ctx.rotate(angle);
+
+    ctx.beginPath();
+
+    ctx.arc(0, 0, 20, 0, 2 * Math.PI, false);
+    ctx.fillStyle = color;
+    ctx.fill();
+
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+
+    drawGun();
+
+    ctx.restore();
+}
+function drawHand(count) {
+    if (count === 1) {
+        
+    } else if (count === 2) {
+
+    } else {
+
+    }
+}
+
+function drawGun() {
+    ctx.beginPath();
+
+    ctx.arc(15, 0, 5, Math.PI/2, 3*Math.PI/2);
+    ctx.lineTo(30, -5);
+    ctx.arc(30, 0, 5, 3*Math.PI/2, Math.PI/2);
+    ctx.lineTo(15, 5);
+    ctx.fillStyle = "brown";
+    ctx.fill();
+
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = "black";
+    ctx.stroke();
 }
 
 function updatePosition() {
@@ -83,8 +131,8 @@ function updatePosition() {
     }
     
     //update pos
-    clientEntity.x += velocity.x;
-    clientEntity.y += velocity.y;
+    clientPos.x += velocity.x;
+    clientPos.y += velocity.y;
 
     //drag
     velocity.x *= 0.8;
@@ -93,6 +141,8 @@ function updatePosition() {
     if (Math.abs(velocity.x) < 1) velocity.x = 0;
     if (Math.abs(velocity.y) < 1) velocity.y = 0;
 
+    //update mouse angle
+    mouseAngle = Math.atan2(clientPos.y-mousePos.y, mousePos.x-clientPos.x);
 }
 
 window.addEventListener("keydown", (key) => {
@@ -104,6 +154,8 @@ window.addEventListener("keydown", (key) => {
         case "s" : keyPresses.down = true;
         break;
         case "d" : keyPresses.right = true;
+        break;
+        case "t" : console.log("open chat!");
         break;
     }
 });
@@ -121,13 +173,23 @@ window.addEventListener("keyup", (key) => {
     }
 });
 
-socket.on("init", (id) => {
-    console.log("Initializing...");
-    clientId = id;
+window.addEventListener("mousemove", (mouse) => {
+    mousePos.x = mouse.clientX;
+    mousePos.y = mouse.clientY;
+
 });
 
-socket.on("getpos", () => {
-    socket.emit("pos", (clientEntity));
+document.querySelector("#canvas").addEventListener("click", (click) => {
+    console.log("click!");
+});
+
+socket.on("init", (id) => {
+    clientId = id;
+    console.log(`Joined with id: ${clientId}`);
+});
+
+socket.on("getplayer", () => {
+    socket.emit("playerdata", clientPos, mouseAngle);
 });
 
 socket.on("allpos", (entitites) => {
